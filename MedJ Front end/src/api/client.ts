b238921840bdc84eafa4d85907f440efe43aed8e
AtanasU <rei1989@abv.bg>
2026-06-14 14:54:25 +0300

    Final changes

--- MedJ Front end/src/api/client.ts
+++ MedJ Front end/src/api/client.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://medj-back-end.onrender.com';
+//const API_BASE_URL = 'https://medj-back-end.onrender.com';
 //For local tests
-// const API_BASE_URL = 'http://localhost:8080';
+ const API_BASE_URL = 'http://localhost:8080';
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
@@ -14,3 +14,4 @@ export const apiClient = axios.create({
 export const getFileUrl = (path: string) => {
   return `${API_BASE_URL}${path}`;
 };
+
